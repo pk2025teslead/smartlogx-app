4785f2913cc2a5dbf7a62c630e7aa7a26213fc22
Pandi0626 <bujipk06262@gmail.com>
2025-12-24 17:29:35 +0530

    Fix Pillow dependency issue and add Python version

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+echo "Python version:"
+python --version
+
+echo "Pip version:"
+pip --version
+
 echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Collecting static files..."
